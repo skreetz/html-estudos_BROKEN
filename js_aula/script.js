baed0433b310916c640d430a6f9c5bb655eb5b1b
Lucas Maia <lucasdocarmomaia@gmail.com>
2025-06-25 19:22:07 -0300

    modified:   js_aula/index.html 	modified:   js_aula/script.js 	modified:   js_aula/style.css

--- js_aula/script.js
+++ js_aula/script.js
@@ -1,27 +1,6 @@
-let a = window.document.getElementById("minhaDiv");
-a.addEventListener('click', clicar)
-a.addEventListener('mouseenter', entrar)
-a.addEventListener('mouseout', sair)
-
-function clicar() {
-    a.innerText = "Clicou!"
-    a.style.background = "red"
+function calcular() {
+    var txtv = document.getElementById('txtvel')
+    var res = document.getElementById('div#res')
+    var vel = Number(txtv.value)
+    res.innerHTML = `A velocidade do seu carro é ${vel} Km/h`
 }
-function entrar() {
-    a.innerText = "Entrou!"
-    a.style.background = "green"
-}
-function sair() {
-    a.innerText = "Saiu!"
-    a.style.background = "blue"
-}
-
-function somar () {
-    var tn1 = document.getElementById("txtn1")
-    var tn2 = document.getElementById("txtn2")
-    var res = document.getElementById("res")
-    var n1 = Number(tn1.value)
-    var n2 = Number(tn2.value)
-    var resultado = n1 + n2
-    res.innerHTML = `A soma entre ${n1} e ${n2} é igual a <strong>${resultado}.</strong>`
-}    
